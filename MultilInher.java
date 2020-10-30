0e685703f6cf300787eff7fca5a9a1999c97948d
Kanakadurga-14 <73577914+Kanakadurga-14@users.noreply.github.com>
2020-10-30 18:22:59 +0530

    Update MultilInher.java

--- MultilInher.java
+++ MultilInher.java
@@ -7,7 +7,7 @@ System.out.println("  Answer the all  the   sections");
 }
 class Daytwo extends Dayone
 {
-public void techni1()
+public void  techni1()
 {
 System.out.println(" explain the concepts with example");
 }
@@ -27,12 +27,9 @@ class MultilInher
 {
 public static void main(String args[])
 {
-Dayone D1 =new Dayone();
-  D1.aptitude();
-Daytwo D2=new Daytwo();
- D2.techni1();
- D2.techni2();
 Daythree D3 =new Daythree();
+ D3.aptitude();
+  D3. techni1()
  D3.hr();
 }
 }
